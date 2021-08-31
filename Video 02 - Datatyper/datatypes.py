8f96e2e72f58a6bb728befb9a611dad5da4a6cad
Lars Emil Knudsen <larseknu@hiof.no>
2021-08-31 08:29:56 +0200

    Oppdatert datatyper-eksempelet

---
+++ Video 02 - Datatyper/datatypes.py
@@ -0,0 +1,19 @@
+# Dette er en string-variabel
+first_name = "Lars Emil"
+# int
+my_age = 29
+# float
+hourly_salary = 321.68
+# int
+hours_worked_year = 1789
+# bool (Boolean)
+full_time_employee = True
+
+# Regner ut årlig lønn og setter dataene i variabelen yearly_salary
+yearly_salary = hourly_salary * hours_worked_year
+
+# Printer ut navn sammensatt med en string og årlig lønn
+print(first_name, " has a yearly salary of ", int(yearly_salary))
+
+# Printer ut datatypen til variabelen hourly_salary
+print(type(hourly_salary))
