9f8eed278ca14dcc6ccf06e42c8f8fd82b058b91
Lars Emil Knudsen <larseknu@hiof.no>
2021-09-03 12:53:52 +0200

    Eksempler fra Video 03 - Operatorer

---
+++ Video 03 - Operatorer/operators.py
@@ -0,0 +1,31 @@
+print(5 + 4)
+
+number_1 = 5
+print(number_1 + 4)
+
+number_2 = 4
+print(number_1 + number_2)
+print(number_1 - 1 + number_2)
+
+first_name = "Lars Emil"
+last_name = "Knudsen"
+full_name = first_name + " " + last_name
+print(full_name)
+
+number_1 = 18
+number_2 = 4
+print(number_1 * number_2)
+
+print(number_1 / number_2)
+
+print(number_1 ** number_2)
+
+print(number_1 // number_2)
+
+print(number_1 % number_2)
+
+x = 5
+print(f"x: {x}")
+
+x += 4  # x = x + 4
+print(f"x: {x}")
